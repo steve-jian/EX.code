3b509c983be98e77df0b2c4b70e48f795a34b90b
steve5631 <alen2048@outlook.com>
2020-04-08 16:33:59 +0800

    Add axois request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>{{ getMessage() }}</h1>
+    <h2>{{ message }}</h2>
+    <button @click="asyncMessage">Click me</button>
+    <button @click="local">Show location</button>
+    <h2>
+      {{ location }}
+    </h2>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      message: "",
+      location: "",
+    };
+  },
+  methods: {
+    getMessage() {
+      return "Hi I am a sync message";
+    },
+    asyncMessage() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("I am an async message");
+        }, 1000);
+      }).then((res) => {
+        this.message = res;
+      });
+    },
+    local() {
+      let api =
+        "https://api.mapbox.com/geocoding/v5/mapbox.places/yunlin.json?access_token=pk.eyJ1Ijoic3RldmUxOTk5IiwiYSI6ImNrOHAxMGFyNzFiaG4zbGwzNjM5dzVmM3UifQ.Y1r0Pt22JU9aMno4_mcSAA";
+      return this.axios.get(api).then((res) => {
+        let place = res.data.features[0].place_name;
+        let coordinate = `${res.data.features[0].center[1]}, ${res.data.features[0].center[0]}`;
+        this.location = `${place} at ${coordinate}`;
+      });
+    },
+  },
 };
 </script>
